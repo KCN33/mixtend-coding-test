e51cfab64170c21195c259ecbfb351601e24881f
KCN33 <m.kaori25@gmail.com>
2022-10-23 20:51:33 +0900

    追加

---
+++ resources/js/components/Calendar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <p>{{ args }}</p>
+        <p>aaaaaaaaaaaaa</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'calendars',
+  props: {
+    args: {},
+  },
+}
+
+// import Vue from "vue/types/umd";
+
+// export default class Calendar extends Vue {
+//   @Prop({
+//     required: true,
+//   })args!: any;
+// }
+</script>
