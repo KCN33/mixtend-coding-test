6a9b793cc92e232733c525cd295b8b85e0f2791a
KCN33 <m.kaori25@gmail.com>
2022-10-24 09:28:39 +0900

    色を設定

--- resources/js/components/Calendar.vue
+++ resources/js/components/Calendar.vue
@@ -11,10 +11,15 @@
           </thead>
           <tbody>
             <tr v-for="hour in args.data.working_hours" :key="hour">
-              <th>{{ hour }}</th>
+              <th>
+                <p v-if="hourOnly(hour)">{{ hour }}</p>
+              </th>
               <td v-for="day in args.data.meeting_days" :key="day">
                 <div v-for="meeting in args.data.meetings[day]" :key="meeting">
-                  <p v-if="meetingIsBetween(hour, meeting.start, meeting.end)">{{ meeting.summary }}</p>
+                  <div class="meeting" v-if="meetingIsBetween(hour, meeting.start, meeting.end)">
+                    <div v-if="meetingStart(hour, meeting.start)">{{ meeting.summary }}</div>
+                    <div v-else>&nbsp;</div>
+                  </div>
                 </div>
               </td>
             </tr>
@@ -33,6 +38,22 @@ export default {
     args: {},
   },
   methods: {
+    hourOnly(hour) {
+      const hours = hour.split(':');
+      return hours[1] == "00";
+    },
+    meetingStart(hour, start) {
+      const hours = hour.split(':');
+      const startHours = start.split(':');
+
+      if (Number(hours[0]) == Number(startHours[0])) {
+        if (Number(hours[1]) == Number(startHours[1])) {
+          // start
+          return true;
+        }
+      }
+      return false;
+    },
     meetingIsBetween(hour, start, end) {
       const hours = hour.split(':');
       const startHours = start.split(':');
